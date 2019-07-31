7f3df4da384891597ccd772d1ce4468370d866eb
Liza Alekseeva <liakknn1@mail.ru>
2019-07-31 17:12:27 +0400

    C1 для проверки

---
+++ HelloWord-CMake/01-basics/C1.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, const char *argv[]) {
+    system("chcp 65001");
+    double x1, y1, z1, x2, y2, z2;
+    if (argc == 7) {
+        x1 = atof(argv[1]);
+        x2 = atof(argv[2]);
+        y1 = atof(argv[3]);
+        y2 = atof(argv[4]);
+        z1 = atof(argv[5]);
+        z2 = atof(argv[6]);
+    } else {
+        puts("Введите координаты точек: x1 y1 z1 x2 y2 z2");
+        scanf("%lf %lf %lf %lf %lf %lf", &x1, &y1, &z1, &x2, &y2, &z2);
+        float distance = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2) + pow((z2 - z1), 2));
+        printf("%lf\n", distance);
+    }
+    printf("%s\n", argv[0]);
+}
