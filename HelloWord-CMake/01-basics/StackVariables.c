a9c0273a19a8efdfb5d5193e7ec6339b11f93eb8
Liza Alekseeva <liakknn1@mail.ru>
2019-08-02 21:16:27 +0400

    Update StackVariables.c

--- HelloWord-CMake/01-basics/StackVariables.c
+++ HelloWord-CMake/01-basics/StackVariables.c
@@ -4,7 +4,7 @@ int main() {
 	int a = 0; 
 	int b = 1; 
 	int c = 2;
-	printf("%llu %llu %llu\n", (unsigned long long) & a, (unsigned long long) & b, (unsigned long long) & c);
+	printf("%lu %lu %lu\n", (unsigned long)(unsigned long long) &a, (unsigned long)(unsigned long long) &b, (unsigned long)(unsigned long long) &c);
 	printf("%d %d %d\n", a, b, c);
 	int* d = &b - 8;
 	*d = 5;
