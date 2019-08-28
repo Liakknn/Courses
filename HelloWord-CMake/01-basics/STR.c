9ac9f9537c214225d0be363a92bc4ebc03f33d3e
Liza Alekseeva <liakknn1@mail.ru>
2019-08-29 00:30:32 +0400

    Update STR.c

--- HelloWord-CMake/01-basics/STR.c
+++ HelloWord-CMake/01-basics/STR.c
@@ -8,7 +8,7 @@ size_t my_strnlen(const char *str) {
     return count;
 }
 
-char *my_strcpy(char *str1, const char *str2) {
+char *my_strcpy(char *str1, char *str2) {
     size_t i = 0;
     while (1) {
         str1[i] = str2[i];
@@ -20,7 +20,7 @@ char *my_strcpy(char *str1, const char *str2) {
     return str1;
 }
 
-char *my_strncpy(char *dest, const char *src, size_t n) {
+char *my_strncpy(char *dest, char *src, size_t n) {
     size_t i;
     for (i = 0; i < n && src[i] != '\0'; i++) {
         dest[i] = src[i];
@@ -32,13 +32,12 @@ char *my_strncpy(char *dest, const char *src, size_t n) {
 }
 
 int main() {
-    char *str1 = "Hello";
-    char *str2[40];
+    char str1[] = "Hello";
+    char str2[] = "liza";
     //printf("%d", my_strnlen(str1));
     my_strcpy(str2, str1);
-    printf("%s", str2);
-    my_strncpy(str1, str2, 3);
-    printf("%s", str1);
+    printf("%s\n", str2);
+    printf("%s", my_strncpy(str1, str2, 2));
 }
 
 
