243e225dcfc39a9dc2cfd3b4fec36e687d534062
Liza Alekseeva <liakknn1@mail.ru>
2019-09-01 00:10:41 +0400

    read_double добавлен

--- HelloWord-CMake/02-functions/SafeRead.c
+++ HelloWord-CMake/02-functions/SafeRead.c
@@ -13,7 +13,7 @@ int read_int(const char *prompt, int min, int max) {
         if (strchr(buf, '\n') == NULL) {
             int ch;
             bool flag = true;
-            while ((ch = fgetc(stdin)) != EOF) {
+            while ((ch = fgetc(stdin)) != '\n' && ch != EOF) {
                 if (!isspace(ch)) {
                     flag = false;
                 }
@@ -26,6 +26,7 @@ int read_int(const char *prompt, int min, int max) {
         char *endptr = NULL;
         errno = 0;
         int l = strtol(buf, &endptr, 10);
+        int tmp = errno;
         if (buf == endptr) {
             printf("Введенная строка - не число!\n");
             continue;
@@ -41,19 +42,70 @@ int read_int(const char *prompt, int min, int max) {
             printf("Введенная строка  - не число!\n");
             continue;
         }
-        if (errno == ERANGE) {
+        if (tmp == ERANGE) {
             printf("Строка выходит за допустимые пределы!\n");
             continue;
         }
         if (l >= min && l <= max) {
             return l;
         } else {
-            printf("Выход за границы диапазона, ожидаемые значения: min - %d, max - %d.\n", min, max);
+            printf("Выход за границы ожидаемого диапазона[%d; %d].\n", min, max);
+        }
+    }
+}
+
+double read_double(const char *prompt, double min, double max) {
+    while (true) {
+        printf("%s", prompt);
+        char buf[32];
+        fgets(buf, sizeof(buf), stdin);
+        if (strchr(buf, '\n') == NULL) {
+            double ch;
+            bool flag = true;
+            while ((ch = fgetc(stdin)) != '\n' && ch != EOF) {
+                if (!isspace(ch)) {
+                    flag = false;
+                }
+            }
+            if (flag == false) {
+                printf("Введенная строка превышает допустимый размер!\n");
+                continue;
+            }
+        }
+        char *endptr = NULL;
+        errno = 0;
+        double l = strtod(buf, &endptr);
+        int tmp = errno;
+        if (buf == endptr) {
+            printf("Введенная строка - не число!\n");
+            continue;
+        }
+        bool flag = true;
+        for (char *p = endptr; *p != '\0'; ++p) {
+            if (!isspace(*p)) {
+                flag = false;
+                break;
+            }
+        }
+        if (!flag) {
+            printf("Введенная строка  - не число!\n");
+            continue;
+        }
+        if (tmp == ERANGE) {
+            printf("Строка выходит за допустимые пределы!\n");
+            continue;
+        }
+        if (l >= min && l <= max) {
+            return l;
+        } else {
+            printf("Выход за границы ожидаемого диапазона[%g; %g].\n", min, max);
         }
     }
 }
 
 int main() {
     int i = read_int("Введите n: ", -100, 100);
-    printf("Успешно: %d", i);
+    printf("Успешно: %d\n", i);
+    double j = read_double("Введите число: ", -100.55, 100.66);
+    printf("успешно: %g\n", j);
 }
