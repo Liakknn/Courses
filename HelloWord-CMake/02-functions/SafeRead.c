e88063c83b55481cf3306ba1b1775c22269b4380
Liza Alekseeva <liakknn1@mail.ru>
2019-09-02 19:18:46 +0400

    Update SafeRead.c

--- HelloWord-CMake/02-functions/SafeRead.c
+++ HelloWord-CMake/02-functions/SafeRead.c
@@ -39,17 +39,17 @@ int read_int(const char *prompt, int min, int max) {
             }
         }
         if (!flag) {
-            printf("Введенная строка  - не число!\n");
+            printf("Введенная строка - не число!\n");
             continue;
         }
         if (tmp == ERANGE) {
-            printf("Строка выходит за допустимые пределы!\n");
+            printf("Результат слишком большой!\n");
             continue;
         }
         if (l >= min && l <= max) {
             return l;
         } else {
-            printf("Выход за границы ожидаемого диапазона[%d; %d].\n", min, max);
+            printf("Выход за границы ожидаемого диапазона[%d;%d].\n", min, max);
         }
     }
 }
@@ -88,24 +88,58 @@ double read_double(const char *prompt, double min, double max) {
             }
         }
         if (!flag) {
-            printf("Введенная строка  - не число!\n");
+            printf("Введенная строка - не число!\n");
             continue;
         }
         if (tmp == ERANGE) {
-            printf("Строка выходит за допустимые пределы!\n");
+            printf("Результат слишком большой!\n");
             continue;
         }
         if (l >= min && l <= max) {
             return l;
         } else {
-            printf("Выход за границы ожидаемого диапазона[%g; %g].\n", min, max);
+            printf("Выход за границы ожидаемого диапазона[%g;%g].\n", min, max);
         }
     }
 }
+
+char *read_str(const char *prompt) {
+    printf("%s", prompt);
+    int size = 0;
+    int capacity = 16;
+    char *str = malloc(capacity);
+    if (str == NULL) {
+        int ch;
+        while ((ch = fgetc(stdin)) != '\n' && ch != EOF);
+        printf("Не удалось выделить память!\n");
+        return NULL;
+    }
+    int ch;
+    while ((ch = fgetc(stdin)) != '\n' && ch != EOF) {
+        if (size == capacity - 1) {
+            capacity += 16;
+            char* tmp = str;
+            str = realloc(str, capacity);
+            if (str == NULL) {
+                printf("Не удалось выделить память!\n");
+                free(tmp);
+                return NULL;
+            }
+        }
+        str[size] = (char) ch;
+        size++;
+    }
+    str[size] = 0;
+    return str;
+}
+
 
 int main() {
     int i = read_int("Введите n: ", -100, 100);
     printf("Успешно: %d\n", i);
     double j = read_double("Введите число: ", -100.55, 100.66);
     printf("успешно: %g\n", j);
+    char* str = read_str("Введите строку: ");
+    printf("успешно: %s",  str);
+    free(str);
 }
